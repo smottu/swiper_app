76ce05492880961ddd4575546b80c58d135d03a8
EC2 Default User <ec2-user@ip-172-31-7-209.ap-northeast-1.compute.internal>
2023-01-29 05:57:23 +0000

    last

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -6,3 +6,18 @@ $(document).ready(function () {
     $(this).css("color","red");
   });
 });
+
+const opt = {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+}
+
+$(document).on('turbolinks:load', function() {
+    let swiper = new Swiper('.swiper',opt);
+});
